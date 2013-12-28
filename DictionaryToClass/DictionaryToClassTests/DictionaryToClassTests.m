9864df3d98c10479e027ebe6da9cc96356d57ceb
Dracuuula <dracuuulalt@gmail.com>
2013-12-28 19:32:35 +0800

    add complete project

---
+++ DictionaryToClass/DictionaryToClassTests/DictionaryToClassTests.m
@@ -0,0 +1,34 @@
+//
+//  DictionaryToClassTests.m
+//  DictionaryToClassTests
+//
+//  Created by Dracuuula on 13-12-17.
+//  Copyright (c) 2013年 Dracuuula. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DictionaryToClassTests : XCTestCase
+
+@end
+
+@implementation DictionaryToClassTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
